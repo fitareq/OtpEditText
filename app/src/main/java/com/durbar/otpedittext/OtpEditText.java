bbd9ee18e129763189d5fa8ea559e0c17b185a52
fitareq <fitareq@gmail.com>
2022-07-05 15:34:57 +0600

    OtpEditText class added.

---
+++ app/src/main/java/com/durbar/otpedittext/OtpEditText.java
@@ -0,0 +1,107 @@
+package com.durbar.otpedittext;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.Editable;
+import android.util.AttributeSet;
+import android.view.ActionMode;
+import android.view.View;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+public class OtpEditText extends AppCompatEditText {
+    private float mSpace = 24; //24 dp by default, space between the lines
+    private float mNumChars = 4;
+    private float mLineSpacing = 8; //8dp by default, height of the text from our lines
+    private int mMaxLength = 6;
+    private float mLineStroke = 2;
+    private Paint mLinesPaint;
+    private OnClickListener mClickListener;
+
+    public OtpEditText(Context context){
+        super(context);
+    }
+    public OtpEditText(Context context, AttributeSet attrs){
+        super(context, attrs);
+        init(context, attrs);
+    }
+    public OtpEditText(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+    private void init(Context context, AttributeSet attrs) {
+        float multi = context.getResources().getDisplayMetrics().density;
+        mLineStroke = multi * mLineStroke;
+        mLinesPaint = new Paint(getPaint());
+        mLinesPaint.setStrokeWidth(mLineStroke);
+        mLinesPaint.setColor(getResources().getColor(R.color.black));
+        setBackgroundResource(0);
+        mSpace = multi * mSpace; //convert to pixels for our density
+        mLineSpacing = multi * mLineSpacing; //convert to pixels for our density
+        mNumChars = mMaxLength;
+
+        super.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // When tapped, move cursor to end of text.
+                setSelection(getText().length());
+                if (mClickListener != null) {
+                    mClickListener.onClick(v);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        mClickListener = l;
+    }
+
+    @Override
+    public void setCustomSelectionActionModeCallback(ActionMode.Callback actionModeCallback) {
+        throw new RuntimeException("setCustomSelectionActionModeCallback() not supported.");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int availableWidth = getWidth() - getPaddingRight() - getPaddingLeft();
+        float mCharSize;
+        if (mSpace < 0) {
+            mCharSize = (availableWidth / (mNumChars * 2 - 1));
+        } else {
+            mCharSize = (availableWidth - (mSpace * (mNumChars - 1))) / mNumChars;
+        }
+
+        int startX = getPaddingLeft();
+        int bottom = getHeight() - getPaddingBottom();
+
+        //Text Width
+        Editable text = getText();
+        int textLength = text.length();
+        float[] textWidths = new float[textLength];
+        getPaint().getTextWidths(getText(), 0, textLength, textWidths);
+
+        for (int i = 0; i < mNumChars; i++) {
+            //canvas.drawLine(startX, bottom, startX + mCharSize, bottom, mLinesPaint);
+            Paint myPaint = new Paint();
+            myPaint.setColor(Color.rgb(223, 229, 228));
+            //myPaint.setStrokeWidth(2);
+            myPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+            //canvas.drawRect(startX, getHeight()-getPaddingTop(), startX+mCharSize, 5, myPaint);
+            canvas.drawRect(startX, 150, startX+mCharSize, 0, myPaint);
+            if (getText().length() > i) {
+                float middle = startX + mCharSize / 2;
+                canvas.drawText(text, i, i + 1, middle - textWidths[0] / 2, bottom - mLineSpacing, getPaint());
+            }
+            if (mSpace < 0) {
+                startX += mCharSize * 2;
+            } else {
+                startX += mCharSize + mSpace;
+            }
+        }
+    }
+
+
+}
